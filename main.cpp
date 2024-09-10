97a346e91538389f70ffac80f885f7b2b425817c
m.maxkamova <m.maxkamova@newuu.uz>
2024-09-10 12:45:30 +0500

    I c

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+
+    //P1
+//    int intNumber = 23;
+//    float floatNumber = 3.14;
+//    double doubleNumber = 45.1234;
+//    bool boolean = true;
+//    char charName = 'A';
+//    cout << "Value of Integer is " << intNumber << ". Size is " << sizeof(intNumber) << endl;
+//    cout << "Value of Float is " << floatNumber << ". Size is " << sizeof(floatNumber) << endl;
+//    cout << "Value of Double is " << doubleNumber << ". Size is " << sizeof(doubleNumber) << endl;
+//    cout << "Value of Boolean is " << boolean << ". Size is " << sizeof(boolean) << endl;
+//    cout << "Value of Char is " << charName << ". Size is " << sizeof(charName) << endl;
+
+    //P2
+//    string name = "Maftuna";
+//    int id = 240252;
+//    string favSong = "N/A";
+//    string movie = "Interstellar";
+//
+//    cout << name << "\n" << id << "\n" << favSong << "\n" << movie << endl;
+
+    //P3
+
+//    cout << "\"I\'m standing\" on the edge of some crazy cliff.\n What I have to do, I have to catch everybody\\\n if they start to go over the cliff - I mean\\\n\\ if they\'re going I have to come out\n from somewhere and \t catch them.";
+
+    //P4
+//    int a = 189;
+//    char b = 'B';
+//    float fNum1 = a/4.0;
+//    int intNum = b;
+//    float floatNum2 = (a+b)/5;
+//    char charN = b + a/3-7;
+//    cout << fNum1 << " " << intNum << " " << floatNum2 << " " << charN << endl;
+
+    //P5
+//    int sum = 44+12;
+//    int product = 12*44;
+//
+//    cout << "Sum:" << sum << endl;
+//    cout << "Product:" << product << endl;
+
+    //P6
+//    int value1;
+//    int value2;
+//    cin >> value1;
+//    cin >> value2;
+//    int result = value1 + value2;
+//
+//    cout << "Sum:" << value1 << "+" << value2 << "=" << result;
+
+    //P7
+//    int x;
+//    int y;
+//    cin >> x;
+//    cin >> y;
+//    int t = (3* (pow(x, 2.0)+3))/(float)(y/4.0 + 3);
+//    cout << t;
+
+    //P8
+    float diameter;
+    cin >> diameter;
+    float area = 4*3.14*pow(diameter/2.0, 2.0);
+    cout << area;
+
+    return 0;
+}
+
+
